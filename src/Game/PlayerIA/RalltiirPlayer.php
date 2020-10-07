f5a425cb81a2681fbd209bf869924c7fc2c998b3
Marieme NDIAYE <marieme.ndiaye@epita.fr>
2020-10-07 21:39:35 +0200

    play2

--- src/Game/PlayerIA/RalltiirPlayer.php
+++ src/Game/PlayerIA/RalltiirPlayer.php
@@ -15,27 +15,56 @@ class RalltiirPlayer extends Player
     protected $opponentSide;
     protected $result;
 
-    public function getChoice()
-    {
-        $nbsci = $this->result->getStatsFor($this->opponentSide)["scissors"];
-        $nbpap = $this->result->getStatsFor($this->opponentSide)["paper"];
-        $nbrock = $this->result->getStatsFor($this->opponentSide)["rock"];
-
-        $myOpponentLessCurrentChoice = "";
+    public function lessCurrentChoice($getStats){
+        $nbsci = $getStats["scissors"];
+        $nbpap = $getStats["paper"];
+        $nbrock = $getStats["rock"];
 
         if ($nbsci <= $nbpap && $nbsci <= $nbrock)
-            $myOpponentLessCurrentChoice = parent::scissorsChoice();
-        else if ($nbpap < $nbsci && $nbpap < $nbrock)
-            $myOpponentLessCurrentChoice = parent::paperChoice();
-        else
-            $myOpponentLessCurrentChoice = parent::rockChoice();
-        
-        if ($this->result->getLastChoiceFor($this->mySide) === $myOpponentLessCurrentChoice)
-            return $myOpponentLessCurrentChoice;
-
-        else
+            return parent::scissorsChoice();
+        else if ($nbpap <= $nbsci && $nbpap <= $nbrock)
             return parent::paperChoice();
+        else
+            return parent::rockChoice();
+    }
+
+    public function moreCurrentChoice($getStats){
+        $nbsci = $getStats["scissors"];
+        $nbpap = $getStats["paper"];
+        $nbrock = $getStats["rock"];
+
+        if ($nbsci >= $nbpap && $nbsci >= $nbrock)
+            return parent::scissorsChoice();
+        else if ($nbpap > $nbsci && $nbpap > $nbrock)
+            return parent::paperChoice();
+        else
+            return parent::rockChoice();
+    }
+
+    public function getChoice()
+    {
+        $myOpponentLessCurrentChoice = $this->lessCurrentChoice($this->result->getStatsFor($this->opponentSide));
+        $myOpponentMoreCurrentChoice = $this->moreCurrentChoice($this->result->getStatsFor($this->opponentSide));
+        $myOpponentLessCurrentChoice = $this->lessCurrentChoice($this->result->getStatsFor($this->opponentSide));
+        $myOpponentMoreCurrentChoice = $this->moreCurrentChoice($this->result->getStatsFor($this->opponentSide));
+        $myLessCurrentChoice = $this->lessCurrentChoice($this->result->getStatsFor($this->mySide));
+        $myMoreCurrentChoice = $this->moreCurrentChoice($this->result->getStatsFor($this->mySide));
+        $myLastChoice = $this->result->getLastChoiceFor($this->mySide);
+        $myOpponentLastChoice = $this->result->getLastChoiceFor($this->opponentSide);
+
+        if (($myOpponentLastChoice === $myOpponentMoreCurrentChoice) && ($myMoreCurrentChoice !== $myOpponentLastChoice))
+            return $myMoreCurrentChoice;
+
+        if ($myOpponentLastChoice === $myOpponentLessCurrentChoice)
+            return $myOpponentLastChoice;
+
+        if ($myMoreCurrentChoice === $myLastChoice)
+            return $myLessCurrentChoice;
+
         
+        
+
+        return $myMoreCurrentChoice;
         // -------------------------------------    -----------------------------------------------------
         // How to get my Last Choice           ?    $this->result->getLastChoiceFor($this->mySide) -- if 0 (first round)
         // How to get the opponent Last Choice ?    $this->result->getLastChoiceFor($this->opponentSide) -- if 0 (first round)
